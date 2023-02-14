35783c55a412f96463a71cbb5cef1599599f756d
Vivek Paliwar <paliwarvivek@gmail.com>
2023-02-14 17:50:14 +0530

    added no 13 program with solution under assignment-2

--- Assignment-2/assignments-programs-2.js
+++ Assignment-2/assignments-programs-2.js
@@ -285,3 +285,22 @@ function stylishQuote(InputQuoteWithauthor) {
 console.log(
   stylishQuote(`"The unexamined life is not worth living." - Socrates`)
 );
+
+// 13. Verify the email address. In this JavaScript exercise, write a function to determine whether an email address is valid or not.
+// input -> function("somebody@somewhere"))
+// output -> false
+// input -> function("person@company.com"))
+// output -> true
+
+function isEmailValid(inputEmail) {
+  console.log(inputEmail);
+
+  let emailPattern = /^[A-Za-z0-9]+@{1}[a-zA-Z0-9]+\.[A-Za-z]/g;
+
+  console.log(emailPattern.test(inputEmail));
+}
+
+console.log(isEmailValid("somebody@somewhere"));
+console.log(isEmailValid("person@company.com"));
+console.log(isEmailValid("20ceuod026@ddu.sce.in"));
+console.log(isEmailValid("paliwarvivek@gmail.com"));
